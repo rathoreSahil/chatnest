0b150f9e0227e90c1b594ffd56d99d8950ccf461
Sahil Rathore <sahilsinghrathore03@gmail.com>
2024-08-10 19:44:04 +0530

    bug fixes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: "http://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,6 @@ const createSendToken = (user, statusCode, res) => {
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
   user.password = undefined;
-  res.cookie("jwt", token, cookieOptions);
 
   res.status(statusCode).json({
     status: "success",
